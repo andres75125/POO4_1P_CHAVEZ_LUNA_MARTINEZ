2073a07abf9583eafe08471d22f632827652dacd
Samantha-Luna-Malta <samluna2406@gmail.com>
2026-07-14 01:43:39 -0500

    Agregar Rol al main

---
+++ src/Rol.java
@@ -0,0 +1,16 @@
+/**
+ * Enumeracion que define los roles de usuario disponibles en el sistema.
+ * * @author Jorge Andres Martinez Gutierrez
+ * @author 
+ * @version 1.0
+ */
+public enum Rol {
+    /**
+     * Rol para aficionados que adquieren boletos o paquetes.
+     */
+    AFICIONADO,
+    /**
+     * Rol para administradores u organizadores de los eventos de la FIFA.
+     */
+    ORGANIZADOR
+}
